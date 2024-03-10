cb64dceaa3d15e26b3761022d718427035c9e19a
Tatiana <tatyana.bolshakova.1990@mail.ru>
2024-03-10 13:53:03 +0700

    Level_2_lesson_4

---
+++ src/Level_2_Lesson_4/ClientGUI.java
@@ -0,0 +1,134 @@
+package Level_2_Lesson_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class ClientGUI extends JFrame implements ActionListener,
+        Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("Tatiana");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    private ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        JScrollPane scrollUsers = new JScrollPane(userList);
+        String[] users = {"user1", "user2", "user3", "user4", "user5", "user6",
+                "user_with_an_exceptionally_long_nickname"};
+        userList.setListData(users);
+        scrollUsers.setPreferredSize(new Dimension(100, 0));
+        cbAlwaysOnTop.addActionListener(this);
+        btnSend.addActionListener(this);
+        tfMessage.addActionListener(this);
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUsers, BorderLayout.EAST);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend || src == tfMessage) {
+            sendMessage();
+        } else {
+            throw new RuntimeException("Undefined source: " + src);
+        }
+    }
+
+    //отправка сообщения
+    private void sendMessage() {
+        String message = tfMessage.getText();
+        String username = tfLogin.getText();
+        if ("".equals(message)) return;
+        tfMessage.setText(null);
+        addLog(String.format("%s: %s", username, message));
+        sendToFile(message, username);
+    }
+
+    //сохранение сообщений в файлах
+    int count = 0;
+    private void sendToFile(String message, String username) {
+        try {
+            PrintStream ps = new PrintStream(new FileOutputStream(count + "log_message.txt"));
+            count++;
+            ps.println(username + "- " + message +"\n");
+            ps.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // добавление сообщения в лог
+    private void addLog(String message) {
+        if ("".equals(message)) return;
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(message+ "\n");
+            }
+        });
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        StackTraceElement[] ste = e.getStackTrace();
+        String msg = String.format("Exception in thread \"%s\" %s: %s\n\tat %s",
+                t.getName(), e.getClass().getCanonicalName(),
+                e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+}
